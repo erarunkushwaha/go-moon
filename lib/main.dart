0746b928b053234ee7f05297d4cd7497b5551bd0
Arun Kushwaha <er.arunkushwaha@gmail.com>
2022-09-23 07:43:03 +0545

    image and title added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:go_moon/pages/home_page.dart';
 
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unnecessary_string_escapes, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,11 +8,35 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-            image: DecorationImage(
-          image: AssetImage(""),
-        )),
+      body: SafeArea(
+        child: Container(
+          child: _pageTitle(),
+        ),
+      ),
+    );
+  }
+
+  Widget _pageTitle() {
+    return const Text(
+      "Go Moon",
+      textAlign: TextAlign.center,
+      overflow: TextOverflow.ellipsis,
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        color: Colors.white,
+        fontSize: 70,
+      ),
+    );
+  }
+
+  Widget _moonImage() {
+    return Container(
+      decoration: BoxDecoration(
+        // color: const Color(0xff7c94b6),
+        image: const DecorationImage(
+          image: AssetImage('assets/images/moon.png'),
+          fit: BoxFit.contain,
+        ),
       ),
     );
   }
