312b38126787ff23d563837284d0508dbc8bf7d9
Arun Kushwaha <er.arunkushwaha@gmail.com>
2022-09-24 08:42:46 +0545

    dropwownd added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,14 +3,21 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  late double _deviceWidth, _deviceHeight;
+  HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       body: SafeArea(
         child: Container(
-          child: _pageTitle(),
+          height: _deviceHeight,
+          width: _deviceWidth,
+          padding: EdgeInsets.symmetric(horizontal: _deviceWidth * 0.05),
+          // color: Colors.amberAccent,
+          child: _moonStationDropDownWidget(),
         ),
       ),
     );
@@ -38,6 +45,18 @@ class HomePage extends StatelessWidget {
           fit: BoxFit.contain,
         ),
       ),
+    );
+  }
+
+  Widget _moonStationDropDownWidget() {
+    List<DropdownMenuItem<String>> _items =
+        ["sector 32", "sector 7", "sector 24"].map(
+      (e) {
+        return DropdownMenuItem(child: Text(e), value: e);
+      },
+    ).toList();
+    return Container(
+      child: DropdownButton(onChanged: (_) {}, items: _items),
     );
   }
 }
